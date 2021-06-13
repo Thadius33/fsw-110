354efa10039d696fa48ec704c10a0fad83a6be0a
Thadius33 <81531017+Thadius33@users.noreply.github.com>
2021-06-12 21:58:33 -0400

    Add files via upload

---
+++ week5/modElement.js
@@ -0,0 +1,6 @@
+function sub(){
+    var firstname = (document.getElementById("fname").value);
+    var middlename = (document.getElementById("mname").value);
+    var lastname = (document.getElementById("lname").value);
+    alert(firstname + "  " + middlename + "  " + lastname )
+}
